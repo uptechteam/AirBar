37ec55911855ff0ec66a61866dd4d1543730abcb
Evgeny Matviyenko <evgeny.matviyenko@uptech.team>
2017-02-24 11:05:30 +0200

    Initial commit.

---
+++ AirBar/AirBar.h
@@ -0,0 +1,19 @@
+//
+//  AirBar.h
+//  AirBar
+//
+//  Created by Evgeny Matviyenko on 2/24/17.
+//  Copyright © 2017 uptechteam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AirBar.
+FOUNDATION_EXPORT double AirBarVersionNumber;
+
+//! Project version string for AirBar.
+FOUNDATION_EXPORT const unsigned char AirBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AirBar/PublicHeader.h>
+
+
